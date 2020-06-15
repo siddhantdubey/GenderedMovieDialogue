7ff853e6cd1bb792ed60bafac1c22b361143b23c
siddhantdubey <sid12.dubey@gmail.com>
2020-06-15 18:24:05 -0400

    Added the position of the character in the credits at the end of the joint file

--- joindata.py
+++ joindata.py
@@ -5,7 +5,7 @@ with open(r'data/movie_lines.txt') as movie_lines:
     for line in movie_lines: #iterate through the lines in the text file
         line = line.strip() #remove newline character
         line_num, chr_id, movie_id, chr_name, line_txt = line.split('+++$+++')
-        line_list = [line_num, chr_id, movie_id, chr_name,0, line_txt]
+        line_list = [line_num, chr_id, movie_id, chr_name,0, line_txt, 0]
         for i in range(1):
             e = str(line_list[i])
             e = e.replace(' ','')        
@@ -14,6 +14,7 @@ with open(r'data/movie_lines.txt') as movie_lines:
 #The following loop creates a dictionary named user_gender, where the keys are the character ids and the values are the genders. 
 with open(r'data/movie_characters_metadata.txt') as chr_data:
     user_gender = {}
+    user_credit = {}
     for line in chr_data:
         line = line.strip()
         chr_id, name, movie_id, movie_name, gender, credit_order = line.split('+++$+++')
@@ -21,11 +22,11 @@ with open(r'data/movie_characters_metadata.txt') as chr_data:
         gender = gender.replace(' ','')
         gender = ' ' + gender
         user_gender[chr_id] = gender
-
+        user_credit[chr_id] = credit_order
 #Loop through the the sublists in big_list
 for i in range(len(big_list)): 
     big_list[i][4] = user_gender[big_list[i][1].replace(' ','')] #Replace the zero in the sublist with the correct gender 
-
+    big_list[i][6] = user_credit[big_list[i][1].replace(' ', '')]
 #Create a new file collated_data.txt if it doesn't exist.Else open it to write to.
 with open(r'data/collated_data.txt','w+') as fout:
     #Loop through each sublist in big list
